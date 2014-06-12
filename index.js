870118508e84453149a7ad656a7fb8960f00b886
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 06:18:44 -0700

    abs dir

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var map = require('lodash.map')
 var template = require('lodash.template')
 
-var file = fs.readFileSync('./template.js', 'utf8')
+var file = fs.readFileSync(__dirname + '/template.js', 'utf8')
 var tmpl = template(file)
 
 module.exports = Compile
